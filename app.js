2f2d2c0d1123b16c6892fed0d2e64d1b1cf1d68e
Akkas Singh <akkassingh@gmail.com>
2017-07-25 23:38:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var request = require("request");
 
 request({
-  uri: "http://www.cjihrig.com/development/php/hello_form.php",
+  url: "http://www.cjihrig.com/development/php/hello_form.php",
   method: "POST",
   form: {
-    name: "Bob"
+    name: "Akkas"
   }
 }, function(error, response, body) {
   console.log(body);
