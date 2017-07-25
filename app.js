0b31d3135c6f298b042960e93dedf5803ed2dd01
akkasnoorsingh.m <akkasnoorsingh.m@.com>
2017-07-25 23:37:13 +0530

    adding app.js and package.json

---
+++ app.js
@@ -0,0 +1,11 @@
+var request = require("request");
+
+request({
+  uri: "http://www.cjihrig.com/development/php/hello_form.php",
+  method: "POST",
+  form: {
+    name: "Bob"
+  }
+}, function(error, response, body) {
+  console.log(body);
+});
